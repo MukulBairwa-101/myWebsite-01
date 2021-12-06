11644844dbf735e81f53c0573d262942233e3dad
Mukul <mukul.bairwa@infilytics.in>
2021-12-06 18:15:43 +0530

    Navigation and styling

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -17,8 +17,8 @@ const Navigation = () => {
                 <li className="list_item">Home</li>
                 <li className="list_item">Projects</li>
                 <li className="list_item">contact us</li>
-                <li className="list_item" onClick={handleDropdown}>more
-                <ul className= {drop?"":"links_v"} >
+                <li className="list_item" onClick={handleDropdown} >more
+                <ul className= {drop?"links_v_drop":"links_v"} >
                      <li className="dropdown_list_item">NRI</li>
                      <li className="dropdown_list_item">FACILITY MANAGEMENT</li>
                      <li className="dropdown_list_item">RESALE AND RENTEL</li>
